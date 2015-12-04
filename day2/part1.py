efad25348bbc2d5572493023acae761087a07295
m3libea <rociorommor@gmail.com>
2015-12-04 00:30:14 -0800

    Second Day: Part1

---
+++ day2/part1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+file = open("input.txt").read().split("\n")
+
+total = 0
+
+for line in file:
+	values = line.split("x")
+	l = int(values[0])
+	w = int(values[1])
+	h = int(values[2])
+	smallest_side = sorted([l,w,h])[:2]
+	area = 2*l*w + 2*w*h + 2*h*l
+	parcial = area + smallest_side[0]*smallest_side[1]
+	total+=parcial
+
+print total
+
+
